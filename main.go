996906e93ecd50c39f218c5cf224c48fc5689f4f
Aibar-ka <fasttasht@gmail.com>
2025-10-02 22:43:29 +0500

    practice 2

--- main.go
+++ main.go
@@ -3,8 +3,12 @@ package main
 import "fmt"
 
 func main() {
+<<<<<<< HEAD
 
 	fmt.Println("My name is Aibar")
 	fmt.Println()
 	fmt.Println("Hello, my name is Khamitov Aibar")
+=======
+	fmt.Println("Hello from practice 2")
+>>>>>>> e3e2f9c (practice 2)
 }
